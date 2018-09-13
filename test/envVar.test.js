82d1dea99025cd50eb78d23d23bfa6d6122e79c6
orthagonal <orthagonal1@gmail.com>
2018-09-13 18:20:39 +0000

    merge/test

--- test/envVar.test.js
+++ test/envVar.test.js
@@ -25,8 +25,8 @@ test('logfmt plugin will display ansi colors when color is true', (t) => {
   });
   log(['color1', 'gray1', 'gray2', 'redpaint'], { msg: 'testing123', tag: { test: { test: '123' } } });
   log(['color2', 'gray1', 'gray2', 'redpaint'], { msg: 'testing123', tag: { test: { test: '123' } } });
-  t.match(logs, ['level=INFO msg="testing123" tag="color1,gray1,gray2,\u001b[31mredpaint\u001b[39m" \u001b[34mtag.test\u001b[39m="\u001b[37m{\'test\':\'123\'}\u001b[39m"',
-    'level=INFO msg="testing123" tag="color2,gray1,gray2,\u001b[31mredpaint\u001b[39m" \u001b[34mtag.test\u001b[39m="\u001b[37m{\'test\':\'123\'}\u001b[39m"']);
+  t.match(logs, ['level=INFO msg="testing123" tag="\u001b[32mcolor1\u001b[39m,\u001b[90mgray1\u001b[39m,\u001b[90mgray2\u001b[39m,\u001b[31mredpaint\u001b[39m" \u001b[36mtag.test\u001b[39m="\u001b[37m{\'test\':\'123\'}\u001b[39m"',
+    'level=INFO msg="testing123" tag="\u001b[33mcolor2\u001b[39m,\u001b[90mgray1\u001b[39m,\u001b[90mgray2\u001b[39m,\u001b[31mredpaint\u001b[39m" \u001b[36mtag.test\u001b[39m="\u001b[37m{\'test\':\'123\'}\u001b[39m"']);
   console.log = oldConsole;
   t.end();
 });
